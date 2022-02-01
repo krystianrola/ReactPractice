9c3f6e7d9cd96d06555f2ba0f7c5068d0cd913f0
krystianrola <krystianrola96@gmail.com>
2022-02-01 13:21:29 +0100

    netflix frontend part 2

--- netflix-remake/src/Banner.js
+++ netflix-remake/src/Banner.js
@@ -1,35 +1,33 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
 import axios from "./axios";
 import requests from "./Requests";
 
-
 function Banner() {
-    const [movie, setMovie] = useState([]);
-    const [description, setDescription] = useState("");
+  const [movie, setMovie] = useState([]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(requests.fetchNetflixOriginals);
-
-            setMovie( 
-                request.data.results[ Math.floor(Math.random() * request.data.results.length -1) ]
-            )
-
-            return request;
-        }
-
-        fetchData();
-    }, []);
-    
-    console.log(movie)
-
-    function truncate (string, number){
-        return string?.length > number ? string.substr(0, number - 1) + ' ...' : string ;  
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
     }
 
-    // black banner 
-    // https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png
+    fetchData();
+  }, []);
+
+  function truncate(string, number) {
+    return string?.length > number
+      ? string.substr(0, number - 1) + " ..."
+      : string;
+  }
+
+  // black banner
+  // https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png
 
   return (
     <header
@@ -42,17 +40,13 @@ function Banner() {
     >
       <div className="banner_content">
         <h1 className="banner_title">
-            {movie?.title || movie?.name || movie?.original_name}
+          {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">
-            {
-                truncate(description, 150)
-            }
-        </h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 100)}</h1>
       </div>
       <div className="banner_fadeBottom" />
     </header>
--- netflix-remake/src/HomeScreen.js
+++ netflix-remake/src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./HomeScreen.css";
 import {Nav} from "./Nav"
 import {Banner} from "./Banner";
+import { Row } from  "./Row";
+import requests from './Requests';
 
 function HomeScreen() {
   return (
@@ -10,9 +12,40 @@ function HomeScreen() {
 
         <Banner /> 
 
-        {/* banner */}
-        
-        {/* Row */}
+        <Row 
+          title='NETFLIX ORIGINALS'
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+        <Row 
+          title='Trending Now'
+          fetchUrl={requests.fetchTrending}
+        />
+        <Row 
+          title='Top Rated'
+          fetchUrl={requests.fetchTopRated}
+        />
+        <Row 
+          title='Action Movies'
+          fetchUrl={requests.fetchActionMovies}
+        />
+        <Row 
+          title='Comedy Movies'
+          fetchUrl={requests.fetchComedyMovies}
+        />
+        <Row 
+          title='Horror Movies'
+          fetchUrl={requests.fetchHorrorMovies}
+        />
+        <Row 
+          title='Romance Movies'
+          fetchUrl={requests.fetchRomanceMovies}
+        />
+        <Row 
+          title='Documentories'
+          fetchUrl={requests.fetchDocumentaries}
+        />
+
     </div>
   )
 }
